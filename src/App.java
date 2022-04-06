fb73d63375649c78dfbc33ccf1c97bd1605ef3f4
Vitor Felipe Leitenski Delela <vitor.delela@edu.pucrs.br>
2022-04-06 20:08:42 -0300

    iteracoes count

--- src/App.java
+++ src/App.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class App {
+    static int ite = 0;
     public static void main(String[] args) throws Exception {
         ArrayList<Integer> listaDisponiveis = new ArrayList<Integer>(Arrays.asList(100,25,10,5,1));
-        System.out.println(contaTroco(listaDisponiveis, 289, new ArrayList<>()));
+        System.out.println(contaTroco(listaDisponiveis, 334, new ArrayList<>()));
+        System.out.println("Iterações: " + ite);
     }
 
     public static ArrayList<Integer> contaTroco(ArrayList<Integer> listaDisponiveis, int valor, ArrayList<Integer> troco){
+        ite++;
         if(valor==0) return troco;
 
         while(listaDisponiveis.get(0) <= valor) {
